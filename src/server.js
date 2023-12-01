3411715e887de5dd7631a3fac7ccbf91e835fede
Luis Cardoso Queiroz <112897238+luuiscardoso@users.noreply.github.com>
2023-12-01 20:10:18 -0300

    mudando porta

--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@ const app = require('./app');
 const dotenv = require('dotenv');
 
 dotenv.config()
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 3003;
 
 app.listen(PORT, () => {console.log(`Servidor rodando na porta ${PORT}`)});
