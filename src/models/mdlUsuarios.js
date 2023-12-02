a38d41bd52c79bb175294347dc00c3636f134dee
Luis Cardoso Queiroz <112897238+luuiscardoso@users.noreply.github.com>
2023-12-02 14:00:26 -0300

    att

--- src/models/mdlUsuarios.js
+++ src/models/mdlUsuarios.js
@@ -5,37 +5,41 @@ const bcrypt = require('bcrypt');
 require('dotenv').config();
 
 const cadastrarUsuario = async (objUsuario) => {
-    const { nome, email, senha, telefones} = objUsuario;
-    const { numero, ddd} = telefones[0];
-
-    const [emailExistente] = await conexao.execute('SELECT * FROM usuarios WHERE email = ?', [email]);
-
-    if(emailExistente.length > 0){
-        throw new Error();
+    try{
+        const { nome, email, senha, telefones} = objUsuario;
+        const { numero, ddd} = telefones[0];
+    
+        const [emailExistente] = await conexao.execute('SELECT * FROM usuarios WHERE email = ?', [email]);
+    
+        if(emailExistente.length > 0){
+            throw new Error();
+        }
+    
+        const senhaHash = await bcrypt.hash(senha, 10);
+    
+        const data = new Date(Date.now()).toUTCString();
+        const dataUltimoLogin = new Date(Date.now()).toUTCString();
+        
+        const qInsereUsuario = 'INSERT INTO usuarios (nome, senha, email, dataCriacao, dataAtualizacao, tel_numero, tel_ddd) VALUES (?, ?, ?, ?, ?, ?, ?)';
+        const [usuarioCriado] = await conexao.execute(qInsereUsuario, [nome, senhaHash, email, data, dataUltimoLogin, numero, ddd]);
+        
+        const insertId = usuarioCriado.insertId;
+        
+        const token = jwt.sign({email, insertId}, process.env.JWT_KEY, {expiresIn: '30m'});
+        
+        const qBuscaEspecifico = 'SELECT id, dataCriacao, dataAtualizacao FROM usuarios WHERE id = ?';
+        const [usuario] = await conexao.execute(qBuscaEspecifico, [insertId]);
+    
+        return {
+            id: usuario[0].id, 
+            dataCriacao: usuario[0].dataCriacao, 
+            dataAtualizacao: usuario[0].dataAtualizacao,
+            ultimoLogin: usuario[0].dataAtualizacao,
+            token: token
+        };
+    } catch{error}{
+        throw new Error()
     }
-
-    const senhaHash = await bcrypt.hash(senha, 10);
-
-    const data = new Date(Date.now()).toUTCString();
-    const dataUltimoLogin = new Date(Date.now()).toUTCString();
-    
-    const qInsereUsuario = 'INSERT INTO usuarios (nome, senha, email, dataCriacao, dataAtualizacao, tel_numero, tel_ddd) VALUES (?, ?, ?, ?, ?, ?, ?)';
-    const [usuarioCriado] = await conexao.execute(qInsereUsuario, [nome, senhaHash, email, data, dataUltimoLogin, numero, ddd]);
-    
-    const insertId = usuarioCriado.insertId;
-    
-    const token = jwt.sign({email, insertId}, process.env.JWT_KEY, {expiresIn: '30m'});
-    
-    const qBuscaEspecifico = 'SELECT id, dataCriacao, dataAtualizacao FROM usuarios WHERE id = ?';
-    const [usuario] = await conexao.execute(qBuscaEspecifico, [insertId]);
-
-    return {
-        id: usuario[0].id, 
-        dataCriacao: usuario[0].dataCriacao, 
-        dataAtualizacao: usuario[0].dataAtualizacao,
-        ultimoLogin: usuario[0].dataAtualizacao,
-        token: token
-    };
 }
 
 const entrarUsuario = async (dadosUsuario) => {
