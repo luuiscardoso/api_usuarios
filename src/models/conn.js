8806f75411a2b85a6eeb89ea1c3327fc1fb6cd1b
Luis Cardoso <luiscard.queiroz@gmail.com>
2024-08-17 15:31:54 -0300

    Testando variaveis de conexao

--- src/models/conn.js
+++ src/models/conn.js
@@ -1,6 +1,12 @@
 const mysql = require ('mysql2/promise');
 require('dotenv').config();
 
+console.log(process.env.MYSQL_DB)
+console.log(process.env.MYSQL_HOST)
+console.log(process.env.MYSQL_PASSWORD)
+console.log(process.env.MYSQL_PORT)
+console.log(process.env.MYSQL_USER)
+
 const conexao = mysql.createPool({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
